bcdb27e4559a31c8142df8f5295a79d85e8acfd4
Pete Houston <contact@petehouston.com>
2016-08-30 18:36:24 +0700

    Update code.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # tinymce-bootstrap-media
+
 A TinyMCE plugin to insert responsive embedded media source.
+
+## Installation
+
+Just [download the source code](https://github.com/petehouston/tinymce-bootstrap-media/archive/master.zip) and put into the `tinymce/plugins` directory.
+
+## Usage
+
+```js
+tinymce.init({
+
+    plugins: ['bootstrapmedia'],
+    toolbar: 'youtube'
+});
+```
+
+## TODO
+
+* [ ] Add Vimeo.
+* [ ] Add width/height configuration.
+
+## License
+
+Please read the [license](LICENSE) file.
---
+++ plugin.js
@@ -0,0 +1,49 @@
+/**
+ * Plugin Name: TinyMCE Bootstrap Media
+ * Plugin URI: https://github.com/petehouston/tinymce-bootstrap-media
+ * Version: 1.0
+ * Author: Pete Houston
+ * Author URI: http://petehouston.com
+ * Description: A simple TinyMCE plugin to insert Bootstrap responsive media
+ * License: MIT
+ */
+(function (mce) {
+    mce.PluginManager.add('bootstrapmedia', function(editor, url) {
+
+        function formatMedia($url) {
+            return '' +
+                '<div class="embed-responsive embed-responsive-16by9">' +
+                '<iframe class="embed-responsive-item" src="' + $url + '" width=\"100%\"></iframe>' +
+                '</div>';
+        }
+
+        function getYoutubeEmbedUrl($id) {
+            return '//www.youtube.com/embed/' + $id;
+        }
+
+        // Youtube button
+        editor.addButton('youtube', {
+            text: 'Youtube',
+            icon: false,
+            onclick: function() {
+                editor.windowManager.open({
+                    title: 'Insert Youtube Video ID',
+                    body: [
+                        {   type: 'textbox',
+                            name: 'code',
+                            label: 'Video code'
+                        }
+                    ],
+                    onsubmit: function(e) {
+                        if(e.data.code) {
+                            editor.insertContent(formatMedia(getYoutubeEmbedUrl(e.data.code)));
+                        }
+                        else {
+                            mce.activeEditor.windowManager.alert('Please insert Youtube video ID into the box.');
+                        }
+                    }
+                });
+            }
+        });
+    });
+}(tinymce));
